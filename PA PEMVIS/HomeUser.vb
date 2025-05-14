1ac974c6cf06d5dc3ef3fb7f3fc2c11ef8842124
MRayhanAT <117957922+MRayhanAT@users.noreply.github.com>
2025-05-14 15:17:52 +0800

    Update HomeUser.vb

--- PA PEMVIS/HomeUser.vb
+++ PA PEMVIS/HomeUser.vb
@@ -1,3 +1,6 @@
-﻿Public Class HomeUser
-
+Public Class HomeUser
+    Private Sub HomeUser_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        AjukanPijaman.Show()
+        Me.Hide()
+    End Sub
 End Class
